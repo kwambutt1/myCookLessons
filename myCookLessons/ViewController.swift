f65fd770dcd8d4a4eb5617bbcec7a2a04f3981ec
kwambutt1 <kwambutt@googlemail.com>
2022-11-23 10:25:42 +0100

    the myFoods4 files were copied into the myCookLessons directory

---
+++ myCookLessons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFoods4
+//
+//  Created by coder on 25.03.22.
+//  Copyright © 2022 coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
